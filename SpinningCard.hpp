870b445bb69582501cf0cab4bc997f60ca69e0d0
Hapax <MJSilk2@gmail.com>
2014-09-24 00:28:49 +0100

    Initial Commit

---
+++ SpinningCard.hpp
@@ -0,0 +1,58 @@
+// faux-3D card spin effect
+
+// hx::Misc::SpinningCard
+
+// Requires:
+//   C++11
+//   SFML (http://sfml-dev.org/)
+
+#ifndef HX_MISC_SPINNINGCARD_HPP
+#define HX_MISC_SPINNINGCARD_HPP
+
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+namespace hx
+{
+	namespace Misc
+	{
+
+// SpinningCard (v1.0.0)
+class SpinningCard : public sf::Drawable, public sf::Transformable
+{
+public:
+	// pass the sprite that it mimics when constructing the card
+	SpinningCard(const sf::Sprite& sprite);
+
+	// rotate around the y axis (in degrees)
+	void spin(float angleInDegrees);
+
+	// rotate around the y axis (in radians)
+	void spinRadians(float angleInRadians);
+
+	// rotate around the x axis (in degrees)
+	void spinVertically(float angleInDegrees);
+
+	// rotate around the x axis (in radians)
+	void spinVerticallyRadians(float angleInRadians);
+
+	// set depth of fake perspective
+	void setDepth(float depth);
+
+private:
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+	sf::VertexArray m_vertices;
+	const sf::Texture* m_pTexture;
+	sf::FloatRect m_initial;
+	const float m_pi;
+	float m_depth;
+};
+
+	} // namespace Misc
+} // namespace hx
+
+#endif // HX_MISC_SPINNINGCARD_HPP
---
+++ readme.txt
@@ -0,0 +1,13 @@
+Spinning Card
+=============
+
+by Hapax (github.com/hapaxia)
+2014 Golden Eagle Software
+
+SpinningCard is a class that takes an SFML sprite, mimics it using an SFML vertex array, then manipulates it using faux-3D so that it is at an angle.
+
+It can spin either horizontally (around the y axis - default) or vertically (around the x axis) but not both at the same time.
+
+The depth of the fake perspective can be specified, which alters the strength of the effect. At a depth of zero, the effect "spins" in a similar way to just changing the width (or height if spinning vertically)
+
+Requires C++11 and SFML (sfml-dev.org)
